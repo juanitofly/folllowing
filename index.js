d93c1832ae1b7b0ce71e2bdc761bdcbd339872da
Juan Valsangiácomo <juanito@MacBook-Air-de-Juan.local>
2017-01-07 12:08:36 -0300

    change port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 
