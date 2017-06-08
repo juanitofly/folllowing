5332ba561dde634fa1cfdc0428180620ea265077
Juan Valsangiacomo <Juan Valsangiacomo>
2017-06-08 11:11:33 -0300

    sending message

--- index.js
+++ index.js
@@ -5,7 +5,125 @@ var io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 5000;
 
+const PAGE_ACCESS_TOKEN = "EAAI7duXIHv8BAO05SnBa1XO8Mz8ErDl1aExJbis1oW390ZByHRy5KSZAiAub98XP4akfL2YpZCyVkpdJY5vAcVo4V1FCVsyyJ5LXHksIjZCSAe9j7TwiiKAybiIP1Ef6ZBdFkkboz6KTpG33NZC5hxF34FYjUPuV4HFCkEBZCkdpQZDZD";
+
 app.use(express.static('public'));
+
+
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === PAGE_ACCESS_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });  
+}
+
+
+
+
+
+
 
 let user_id = 0;
 
@@ -31,17 +149,6 @@ io.on('connection', function(socket){
 
 http.listen(PORT, function(){
   console.log('NEW listening on *:' + PORT);
-});
-
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === "EAAI7duXIHv8BAOWKB9VoVZBy6FUrf9tBporUBs8aKhTYalRc2PcMQWPvvxWK7P0XSKOAZBf0UR2ZCG7BLyQBZBlwZAC8qiIotwtShLkyphkI0riAk018lJvIvHlqaYc2jJ4ngD5bRNLvjqwY3V3azMzTcfZACLCz0AKNEQJYH2zQZDZD") {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
-  }  
 });
 
 app.get('/domotica/:action', function(req, res) {
